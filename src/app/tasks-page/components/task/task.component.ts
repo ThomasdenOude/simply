cdccc13cc4d93a07d6866bfb082062f6efbfbd0f
Verbinding Maken <aaneen.schakeling@gmail.com>
2024-03-25 19:52:56 +0100

    responsive Touchevent or single click on taskCard to open edit task dialog

--- src/app/tasks-page/components/task/task.component.ts
+++ src/app/tasks-page/components/task/task.component.ts
@@ -4,11 +4,23 @@ import {
 	InputSignal,
 	Output,
 	input,
+	ViewChild,
+	ElementRef,
+	AfterViewInit,
+	OnDestroy,
 } from '@angular/core';
 
 import { MatCardModule } from '@angular/material/card';
 
 import { Task } from '../../models/task.interface';
+import {
+	fromEvent,
+	Observable,
+	Subject,
+	switchMap,
+	takeUntil,
+	timer,
+} from 'rxjs';
 
 @Component({
 	selector: 'app-task',
@@ -17,13 +29,45 @@ import { Task } from '../../models/task.interface';
 	templateUrl: './task.component.html',
 	styleUrl: './task.component.scss',
 })
-export class TaskComponent {
+export class TaskComponent implements AfterViewInit, OnDestroy {
+	private destroy: Subject<void> = new Subject<void>();
 	public task: InputSignal<Task> = input.required<Task>();
 
-	@Output()
-	public editTask = new EventEmitter<Task>();
+	@ViewChild('taskCard', { read: ElementRef })
+	private taskCard!: ElementRef;
 
-	protected onEditTask(): void {
+	@Output()
+	public editTask: EventEmitter<Task> = new EventEmitter<Task>();
+
+	ngAfterViewInit() {
+		const card = this.taskCard.nativeElement;
+		const touchEnd$: Observable<unknown> = fromEvent(card, 'touchend').pipe(
+			takeUntil(timer(300))
+		);
+		fromEvent(card, 'touchstart')
+			.pipe(
+				takeUntil(this.destroy),
+				switchMap(() => touchEnd$)
+			)
+			.subscribe(() => this.emitEditTask());
+
+		const mouseDown$: Observable<unknown> = fromEvent(card, 'mouseup').pipe(
+			takeUntil(timer(300))
+		);
+		fromEvent(card, 'mousedown')
+			.pipe(
+				takeUntil(this.destroy),
+				switchMap(() => mouseDown$)
+			)
+			.subscribe(() => this.emitEditTask());
+	}
+
+	protected emitEditTask(): void {
 		this.editTask.emit(this.task());
+	}
+
+	ngOnDestroy() {
+		this.destroy.next();
+		this.destroy.complete();
 	}
 }
