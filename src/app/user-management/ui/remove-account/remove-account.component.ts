2cb5661e24b8b1c6563ff37e14469a7662fc17e8
Verbinding Maken <aaneen.schakeling@gmail.com>
2024-05-22 13:16:47 +0200

    S-4 Adjusted remove account button for mobile view

--- src/app/user-management/ui/remove-account/remove-account.component.ts
+++ src/app/user-management/ui/remove-account/remove-account.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Signal } from '@angular/core';
 import {
 	MatDialogActions,
 	MatDialogContent,
@@ -6,6 +6,8 @@ import {
 	MatDialogTitle,
 } from '@angular/material/dialog';
 import { MatButton } from '@angular/material/button';
+import { ResponsiveService } from '../../../base/services/responsive.service';
+import { Devices } from '../../../base/models/devices';
 
 @Component({
 	selector: 'simply-remove-account',
@@ -15,9 +17,18 @@ import { MatButton } from '@angular/material/button';
 	styleUrl: './remove-account.component.scss',
 })
 export class RemoveAccountComponent {
+	private responsiveService: ResponsiveService = inject(ResponsiveService);
 	private dialogRef: MatDialogRef<RemoveAccountComponent, boolean> = inject(
 		MatDialogRef<RemoveAccountComponent>
 	);
+
+	protected Devices = Devices;
+
+	protected device: Signal<Devices>;
+
+	constructor() {
+		this.device = this.responsiveService.device;
+	}
 
 	protected cancel(): void {
 		this.dialogRef.close(false);
