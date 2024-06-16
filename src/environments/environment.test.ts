f8e1c4eabd96ec02885e72a1289c8be5826c04ef
Verbinding Maken <aaneen.schakeling@gmail.com>
2024-06-16 12:32:11 +0200

    main Update firebase for local development

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,7 +1,7 @@
 import { Environment } from '../app/base/models/environment.model';
 
 export const environment: Environment = {
-	production: true,
+	production: false,
   baseUrl: 'https://test-simply-task-board.web.app',
 	firebaseConfig: {
     apiKey: "AIzaSyAPhrtGaTIEU5jUF9mRtAN8ME8wpNB9kpY",
